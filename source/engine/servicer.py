5da28ce74163fa683634e4c72112c96ba6f11116
Myeongjin SHIN <audwls031@gmail.com>
2019-07-25 01:03:46 +0900

    implement: redis

--- source/engine/servicer.py
+++ source/engine/servicer.py
@@ -10,17 +10,22 @@ import engine_pb2_grpc #pylint: disable=import-error
 class EngineServicer(engine_pb2_grpc.EngineServicer):
 
     def __init__(self):
-        self.db = dict()
+        redis_kwargs = {
+            'host': os.environ['REDIS_HOST'],
+            'port': int(os.environ['REDIS_PORT']) if 'REDIS_PORT' in os.environ else 6379,
+        }
+        self.train_images = redis.Redis(db=1, **redis_kwargs)
 
     def PutTrainImage(self, image, context):
         if image.id.bytes == b'':
             image.id.bytes = uuid.uuid4().bytes
 
-        self.db[image.id.bytes] = image
+        self.train_images.set(image.id.bytes, image.SerializeToString())
         return image.id
 
     def GetTrainImage(self, id_, context):
-        if id_.bytes not in self.db:
+        image = self.train_images.get(id_.bytes)
+        if image == None:
             return engine_pb2.Image()
         
-        return self.db[id_.bytes]
+        return image
