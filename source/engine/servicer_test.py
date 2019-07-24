61703b6992055a8e0d832510831234734f28d848
Myeongjin SHIN <audwls031@gmail.com>
2019-07-25 00:38:06 +0900

    test

--- source/engine/servicer_test.py
+++ source/engine/servicer_test.py
@@ -21,7 +21,7 @@ class EngineServicerTestCase(unittest.TestCase):
         self.server.stop(0)
 
     def test_train_image(self):
-        id_ = engine_pb2.ID(bytes=uuid.uuid4().bytes))
+        id_ = engine_pb2.ID(bytes=uuid.uuid4().bytes)
         image = engine_pb2.Image(id=id_)
         with grpc.insecure_channel('localhost:50051') as channel:
             stub = engine_pb2_grpc.EngineStub(channel)
