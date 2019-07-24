df5d73f369c12bdba92704c652d4ef2be77d2f83
Myeongjin SHIN <audwls031@gmail.com>
2019-07-25 00:46:56 +0900

    implement

--- source/engine/servicer.py
+++ source/engine/servicer.py
@@ -9,8 +9,18 @@ import engine_pb2_grpc #pylint: disable=import-error
 
 class EngineServicer(engine_pb2_grpc.EngineServicer):
 
-    def PutTrainImage(self, request, context):
-        return engine_pb2.ID()
+    def __init__(self):
+        self.db = dict()
 
-    def GetTrainImage(self, request, context):
-        return engine_pb2.Image()
+    def PutTrainImage(self, image, context):
+        if image.id.bytes == b'':
+            image.id.bytes = uuid.uuid4().bytes
+
+        self.db[image.id.bytes] = image
+        return image.id
+
+    def GetTrainImage(self, id_, context):
+        if id_.bytes not in self.db:
+            return engine_pb2.Image()
+        
+        return self.db[id_.bytes]
