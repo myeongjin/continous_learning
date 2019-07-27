c61c4bfdb3960cf0bc1962b2ecb6361ac47d348a
Myeongjin SHIN <audwls031@gmail.com>
2019-07-28 00:00:14 +0900

    add engine inout pipe

--- source/engine/engine.py
+++ source/engine/engine.py
@@ -1,13 +1,43 @@
+import os
+import threading
+
+import redis
+
+import engine_pb2 # pylint: disable=import-error
 
 
 class Engine:
 
+    def __init__(self):
+        self.should_continue = True
+        self.thread = threading.Thread(target=self.run, daemon=True)
+
+    def run(self):
+        redis_kwargs = {
+            'host': os.environ['REDIS_HOST'],
+            'port': int(os.environ['REDIS_PORT']) if 'REDIS_PORT' in os.environ else 6379,
+        }
+        train_images = redis.Redis(db=1, **redis_kwargs)
+        train_labels = redis.Redis(db=2, **redis_kwargs)
+
+        while self.should_continue:
+            for id_ in train_images.scan():
+                label = train_labels.get(id_)
+                if label == None:
+                    continue
+
+                image = train_images.get(id_)
+                image = engine_pb2.Image.FromString(image)
+                label = engine_pb2.Label.FromString(label)
+
+                import time
+                print(image)
+                print(label)
+                time.sleep(1)
+
     def start(self):
-        pass
+        self.thread.start()
 
     def stop(self):
-        pass
-
-if __name__ == '__main__':
-    engine = Engine()
-    engine.start()
+        self.should_continue = False
+        self.thread.join()
